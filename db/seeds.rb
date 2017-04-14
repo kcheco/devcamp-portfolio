dec2869bcc4b51d667cece56ec7a45773a0e294a
Kelvin Checo <checokelvin@gmail.com>
2017-04-13 23:16:59 -0400

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam tellus, feugiat ac accumsan ut, dapibus ut eros. Aenean quis turpis nibh. Aliquam eu enim ligula. Curabitur faucibus bibendum tellus, vel finibus sem luctus non. Praesent eu sem vel sapien porttitor pulvinar. Curabitur fermentum felis urna, non dignissim ante imperdiet a. Donec aliquam euismod erat non laoreet. Curabitur lorem ligula, porta eu tortor quis, ultricies iaculis turpis. Suspendisse placerat finibus dapibus. Aliquam semper lectus vel vestibulum viverra. Vivamus scelerisque purus aliquam lorem feugiat posuere. Pellentesque elementum arcu lobortis, fringilla ex non, facilisis massa. Phasellus vitae mattis nibh, et lacinia justo. Mauris dolor quam, euismod id vestibulum molestie, malesuada quis ex."
+	)
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+puts "5 skills created"
+
+9.times do |item|
+	Portfolio.create!(
+		title: "Portfolio title #{item}",
+		subtitle: "My great service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam tellus, feugiat ac accumsan ut, dapibus ut eros. Aenean quis turpis nibh. Aliquam eu enim ligula. Curabitur faucibus bibendum tellus, vel finibus sem luctus non. Praesent eu sem vel sapien porttitor pulvinar. Curabitur fermentum felis urna, non dignissim ante imperdiet a. Donec aliquam euismod erat non laoreet.",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200"
+	)
+end
+puts "9 portfolio items created"
